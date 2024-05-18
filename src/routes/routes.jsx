29567cfcd0dd90951900dcc0e214445ca3c9fe7b
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2024-05-18 17:55:14 +0300

    Config routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,29 @@
+import Root from "./Root";
+import Shop from "./Shop";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+import Home from "./Home";
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+];
+
+export default routes;
